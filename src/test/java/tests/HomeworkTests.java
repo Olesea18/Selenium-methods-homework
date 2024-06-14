cb9cf22bfeef86840e32853fb0f25bf75382b7ee
OGAMURARI <olesea.gamurari@orange.com>
2024-06-14 20:11:04 +0300

    Homework commit

--- src/test/java/tests/HomeworkTests.java
+++ src/test/java/tests/HomeworkTests.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
